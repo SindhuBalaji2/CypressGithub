d351debf35d4592c5394963a7e06f6ca887a803e
SindhuBalaji2 <69456757+SindhuBalaji2@users.noreply.github.com>
2020-08-10 23:39:58 +0530

    Main Code

--- github.spec.js
+++ github.spec.js
@@ -2,7 +2,7 @@ context('Cypress github login',() => {
     
 
     it('signup to signout',() =>{
-      cy.screenshot()
+      //cy.screenshot()
       cy.visit('https://github.com/')
       cy.contains('Sign up').click()
       cy.go('back')
@@ -14,7 +14,7 @@ context('Cypress github login',() => {
     
       cy.visit('https://github.com/')
       cy.get('button').first().click()
-      cy.screenshot()
+      //cy.screenshot()
       cy.contains('Sign in').click()
       cy.contains('Username').type('SindhuB25')
       cy.contains('Password').type('Github#21')
@@ -31,20 +31,20 @@ context('Cypress github login',() => {
       cy.wait(2000)
       cy.get('.js-feature-preview-indicator-container > .Header-link > .avatar').click()
       cy.get('.dropdown-item').contains('Your profile').click()
-      cy.screenshot()
+      //cy.screenshot()
       cy.wait(2000)
       cy.get('.js-feature-preview-indicator-container > .Header-link > .avatar').click()
       cy.wait(2000)
       cy.get('[href="/settings/profile"]').click()
-      cy.screenshot()
+      //cy.screenshot()
       cy.contains('Bio').type('I am a Automation Tester')
       cy.contains('Update profile').click()
       cy.wait(2000)
-      cy.screenshot()
+      //cy.screenshot()
       cy.get('.js-feature-preview-indicator-container > .Header-link > .avatar').click()
       cy.wait(2000)
       cy.contains('Sign out').click()
-      cy.screenshot()
+      //cy.screenshot()
     
     })
   
